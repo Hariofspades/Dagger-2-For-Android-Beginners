fadc7100d629969a8c9ae7077767964a1a90ae3e
Hari <hariutd@gmail.com>
2017-11-15 23:09:32 +0530

    @modules and @provides annotation

---
+++ src/main/java/Example_1_Dagger2_alt/Soldiers.java
@@ -0,0 +1,11 @@
+package Example_1_Dagger2_alt;
+
+/**
+ * Created by Hari on 15/11/17.
+ */
+public class Soldiers {
+
+    public Soldiers(){
+
+    }
+}
---
+++ src/main/java/Example_1_Dagger2_alt/Starks.java
@@ -0,0 +1,25 @@
+package Example_1_Dagger2_alt;
+
+import javax.inject.Inject;
+
+/**
+ * Created by Hari on 04/11/17.
+ */
+public class Starks implements House {
+
+    @Inject
+    public Starks(){
+    }
+
+    @Override
+    public void prepareForWar() {
+        //do something
+        System.out.println(this.getClass().getSimpleName()+" prepared for war");
+    }
+
+    @Override
+    public void reportForWar() {
+        //do something
+        System.out.println(this.getClass().getSimpleName()+" reporting..");
+    }
+}
